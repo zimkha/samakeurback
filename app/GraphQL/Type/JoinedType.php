de9977495a49955111997b4a8af5e9b27f0e78d6
khazim ndiaye <zimkhandiaye@gmail.com>
2020-06-04 02:25:23 +0100

    update

--- app/GraphQL/Type/JoinedType.php
+++ app/GraphQL/Type/JoinedType.php
@@ -3,10 +3,12 @@
 namespace App\GraphQL\Type;
 
 use GraphQL;
-use GraphQL\Type\Definition\Type;
-use Rebing\GraphQL\Support\Type as GraphQLType;
 use App\Joined;
 use App\NiveauPlan;
+use App\Outil;
+use Carbon\Carbon;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Type as GraphQLType;
 class JoinedType extends GraphQLType
 {
     protected $attributes = [
@@ -16,24 +18,55 @@ class JoinedType extends GraphQLType
     public function fields(): array
     {
         return [
-            'id'                     => ['type' => Type::id()],
-            'plan_id'                => ['type' => Type::int()],
-            'fichier'                => [ 'type' => Type::string(), 'description' => ''],
-            'active'                 => ['type' => Type::boolean()], 
+
+            'id'                     => [ 'type' => Type::id()],
+            'plan_id'                => [ 'type' => Type::int()],
+            'fichier'                => [ 'type' => Type::string(),],
+            'active'                 => [ 'type' => Type::boolean()], 
             'description'            => [ 'type' => Type::string()],
             'created_at'             => [ 'type' => Type::string()],
             'created_at_fr'          => [ 'type' => Type::string()],
             'updated_at'             => [ 'type' => Type::string()],
             'updated_at_fr'          => [ 'type' => Type::string()],
             'deleted_at'             => [ 'type' => Type::string()],
-            'plan'                   => ['type' => GraphQL::type('Plan')],
-
-            // Resolve 
-           
-
-          
-           
+            'plan'                   => [ 'type' => GraphQL::type('Plan')],           
         ];
+    }
+    protected function resolveCreatedAtField($root, $args)
+    {
+        if (!isset($root['created_at']))
+        {
+            $date_at = $root->created_at;
+        }
+        else
+        {
+            $date_at = is_string($root['created_at']) ? $root['created_at'] : $root['created_at']->format(Outil::formatdate());
+        }
+        return $date_at;
+    }
+    protected function resolveUpdatedAtField($root, $args)
+    {
+        if (!isset($root['updated_at']))
+        {
+            $date_at = $root->updated_at;
+        }
+        else
+        {
+            $date_at = is_string($root['updated_at']) ? $root['updated_at'] : $root['updated_at']->format(Outil::formatdate());
+        }
+        return $date_at;
+    }
+    protected function resolveCreatedAtFrField($root, $args)
+    {
+        if (!isset($root['created_at']))
+        {
+            $created_at = $root->created_at;
+        }
+        else
+        {
+            $created_at = $root['created_at'];
+        }
+        return Carbon::parse($created_at)->format('d/m/Y H:i');
     }
 
   
